904bbe273b721b726a9b0b721ddf6f66079167f3
Lucas Cosolo <lucas@Lucass-MacBook-Pro.local>
2018-01-05 13:51:45 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TweetWatcher for Discord
+
+TweetWatcher is a Discord bot for watching twitter accounts and displaying their tweets in Discord channels.
+
+## TODO
+
+- Switch from JSON storage to a Mongo database
---
+++ index.js
@@ -0,0 +1,185 @@
+Array.prototype.removeIf = function(condition) {
+    var i = this.length;
+    while (i--) {
+        if (condition(this[i], i)) {
+            this.splice(i, 1);
+        }
+    }
+};
+
+const onExit = require('./exit');
+
+const fs = require('fs');
+
+const Twitter = require('twit');
+const Discord = require('discord.js');
+
+const config = require('./config.json');
+
+const twitter = new Twitter(config.twitter);
+const watching = [];
+
+let data = fs.readFileSync('./data.json', 'utf8');
+if (data) {
+    let json = JSON.parse(data);
+    if (json.watching) {
+        json.watching.forEach(watcher => watching.push(watcher));
+    }
+}
+
+function sendTweet(event, embed) {
+    watching.filter(watcher => watcher.userID === event.user.id).forEach(watcher => {
+        discord.channels.filter(channel => channel.id === watcher.channelID).forEach(channel => {
+            channel.send({embed: embed}).catch(console.error);
+        });
+    });
+}
+
+const discord = new Discord.Client();
+
+function setupStream() {
+    if (watching.length > 0) {
+        let stream = twitter.stream('statuses/filter', {
+            follow: watching.map(watcher => watcher.userID)
+        });
+        stream.on('error', console.error);
+        stream.on('tweet', event => {
+            let embed = {
+                color: config.colors.main,
+                author: {
+                    name: event.user.name,
+                    icon_url: event.user.profile_image_url
+                },
+                description: event.text,
+                url: 'https://twitter.com/' + event.user.screen_name + '/status/' + event.id_str,
+                fields: []
+            };
+            if (event.in_reply_to_status_id_str) {
+                twitter.get('statuses/show', {id: event.in_reply_to_status_id_str}, (error, tweet, res) => {
+                    if (!error && tweet) {
+                        embed.fields.push({
+                            name: '@' + tweet.user.screen_name,
+                            value: tweet.text
+                        });
+                    }
+                    sendTweet(event, embed);
+                });
+            } else {
+                sendTweet(event, embed);
+            }
+        });
+    }
+}
+
+discord.on('ready', () => {
+    console.log('Happy birthday!');
+});
+
+discord.on('message', event => {
+    let message = event.content;
+    if (message.startsWith(config.prefix)) {
+        message = message.replace(config.prefix, '');
+        let args = message.split(' ');
+        let command = args[0];
+        args.splice(0, 1);
+        switch (command.toLowerCase()) {
+            case 'watch':
+                if (args.length == 1) {
+                    let name = args[0];
+                    if (name.startsWith('@')) name = name.replace('@', '');
+                    twitter.get('users/show', {screen_name: name}, (error, account, res) => {
+                        if (!error && account) {
+                            watching.push({
+                                userID: account.id_str,
+                                channelID: event.channel.id
+                            });
+                            setupStream();
+                            event.reply('I am now watching ' + account.name + ' in ' + event.channel);
+                        } else {
+                            event.reply('User not found: @' + name);
+                        }
+                    });
+                } else {
+                    event.reply('Error! Usage: `' + config.prefix + 'watch @<screenname>`')
+                }
+                break;
+            case 'unwatch':
+                if (args.length == 1) {
+                    let name = args[0];
+                    if (name.startsWith('@')) name = name.replace('@', '');
+                    twitter.get('users/show', {screen_name: name}, (error, account, res) => {
+                        if (!error && account) {
+                            watching.removeIf(watcher => watcher.userID === account.id_str && watcher.channelID === event.channel.id);
+                            setupStream();
+                            event.reply('I am no longer watching ' + account.name + ' in ' + event.channel);
+                        } else {
+                            event.reply('User not found: @' + name);
+                        }
+                    });
+                } else {
+                    event.reply('Error! Usage: `' + config.prefix + 'watch @<screenname>`')
+                }
+                break;
+            case 'list':
+                let query = watching
+                    .filter(watcher => watcher.channelID === event.channel.id)
+                    .map(watcher => watcher.userID);
+                if (query.length == 0) {
+                    event.reply('No accounts are being watched in this channel! Use `' + config.prefix + 'help` for more information.');
+                    return;
+                }
+                if (query.length > 100) {
+                    query.length = 100;
+                }
+                twitter.post('users/lookup', { user_id: query }, (error, accounts, res) => {
+                    event.reply('Accounts being watched in ' + event.channel + ':\n' +
+                        accounts.map(account => '`@' + account.screen_name + '`').join(', '));
+                });
+                break;
+            case 'help':
+                event.guild.fetchMember(event.author).then(
+                    (member) => {
+                        let name = member.nickname || event.author.username;
+                        event.channel.send({embed: {
+                            color: config.colors.main,
+                            title: 'TweetWatcher Command Index',
+                            description: 'TweetWatcher is a simple bot created by Lucas#5300 to watch tweets from users and display them in discord!',
+                            footer: {
+                                icon_url: event.author.avatarURL,
+                                text: 'Requested by ' + name  
+                            },
+                            fields: [
+                                {
+                                    name: config.prefix + 'help',
+                                    value: 'Display this help message'
+                                },
+                                {
+                                    name: config.prefix + 'watch @<screenname> [channel]',
+                                    value: 'Enable watcher for a twitter account in current [or another] channel'
+                                },
+                                {
+                                    name: config.prefix + 'unwatch @<screenname>',
+                                    value: 'Disable watcher for a twitter account in current [or another] channel'
+                                },
+                                {
+                                    name: config.prefix + 'list',
+                                    value: 'List all watchers for current [or another] channel'
+                                }
+                            ]
+                        }});
+                    },
+                    console.error
+                ).catch(console.error);
+                break;
+        }
+    }
+});
+
+function save() {
+    fs.writeFileSync('data.json', JSON.stringify({watching: watching}), 'utf8');
+}
+
+onExit(save);
+setInterval(save, 1000 * 60 * 5);
+
+discord.login(config.token);
